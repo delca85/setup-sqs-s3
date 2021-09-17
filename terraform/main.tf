78b252f30b145a7929df63500a57a26ca8225e0b
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2021-09-17 21:39:03 +0200

    create local layer

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,26 @@ module "lambda_function" {
   description   = "Create S3 Folder and SQS as destination of S3 events"
   handler       = "create_sqs.create_janus_queue_handler"
   runtime       = "python3.8"
+  publish = true
 
-  source_path = "../src/createSqsAndS3Folder/create_sqs.py"
+  source_path = "../src/createSqsAndS3Folder/create_sqs"
+
+  layers = [
+    module.lambda_layer_local.arn
+  ]
 
   tags = {
     Name = "marvel-lambda"
   }
+}
+
+module "lambda_layer_local" {
+  source = "terraform-aws-modules/lambda/aws"
+  create_layer = true
+
+  layer_name = "sqs-s3-lambda-local-layer"
+  description = "Create S3 Folder and SQS as destination of S3 events function layer (deployed locally)"
+  compatible_runtimes = ["python3.8"]
+
+  source_path = "../src/createSqsAndS3Folder"
 }
